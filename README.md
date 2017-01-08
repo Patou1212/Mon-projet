9ba98ac36b9dd984f9a82f1582e05d406dfc8981
Patou1212 <afannoukevine@gmail.com>
2017-01-08 01:52:18 +0100

    Added routing

---
+++ README.md
@@ -0,0 +1,9 @@
+# Angnewrouter
+
+This app is to demonstrate the usage of the new Angular Router.
+
+For further details - http://tphangout.com/?p=363
+
+This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.9.
+
+
---
+++ src/app/firstpage/firstpage.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-firstpage',
+  templateUrl: 'firstpage.component.html',
+  styleUrls: ['firstpage.component.css']
+})
+export class FirstpageComponent implements OnInit {
+
+  localUser = {
+    username: '',
+    password:''
+  }
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  login() {
+
+    
+  }
+
+}
